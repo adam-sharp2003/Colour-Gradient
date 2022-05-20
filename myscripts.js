271c2070b45c8057be229e10ea7943ffedd7b62b
Adam Sharp <adam.sharp2003@gmail.com>
2022-05-20 13:56:30 +0100

    Added Max Number Field

--- myscripts.js
+++ myscripts.js
@@ -24,12 +24,14 @@ function formChanged() {
     for (let l = 0; l < 3; l += 1)
       for (let i = rgbList[3][l], j = 0; i <= rgbList[5][l], j < slider; i += (rgbList[5][l] - rgbList[3][l]) / slider, j += 1) rgbList[l].push(i);
     for (let l = 0; l < 3; l += 1)
-      for (let i = rgbList[5][l], j = 0; i >= rgbList[4][l], j < (101-slider); i -= (rgbList[5][l] - rgbList[4][l]) / (101-slider), j += 1) rgbList[l].push(i);
+      for (let i = rgbList[5][l], j = 0; i >= rgbList[4][l], j < ((colourCount*10+1)-slider); i -= (rgbList[5][l] - rgbList[4][l]) / ((colourCount*10+1)-slider), j += 1) rgbList[l].push(i);
     return `#${[rgbList[0][Math.round(n*10)],rgbList[1][Math.round(n*10)],rgbList[2][Math.round(n*10)]].map(n=>Math.round(n).toString(16).padStart(2,0)).join("")}`
   };
 
   var textBoxes = []
-  for (let l = 0; l <= 100; l += 1) {
+  var colourCount = document.getElementById("colourCount").value
+  document.getElementById("myRange").max = colourCount*10;
+  for (let l = 0; l <= colourCount*10; l += 1) {
     const para = document.createElement("div");
     const node = document.createTextNode(l / 10);
     const element = document.getElementById("colourMap");
@@ -39,7 +41,7 @@ function formChanged() {
     textBoxes.push("colour" + l)
     document.getElementById(textBoxes[l]).style.backgroundColor = getColour(l / 10);
     document.getElementById(textBoxes[l]).style.margin = 0;
-    document.getElementById(textBoxes[l]).style.fontSize = "6px";
+    document.getElementById(textBoxes[l]).style.fontSize = `${5/(colourCount/10)}px`;
     document.getElementById(textBoxes[l]).style.color = "white";
 
   }
