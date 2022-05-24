45b65df0b9be92e1cffd6e38f6e971a785334629
Adam Sharp <adam.sharp2003@gmail.com>
2022-05-24 14:10:37 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -3,30 +3,30 @@ function formChanged() {
   const getColour = (n) => {
     function hexToRgb(hex) {
       let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      } : null;
+      return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+      ] : null;
     }
     let a = hexToRgb(document.getElementsByName("firstcolour")[0].value)
     let d = hexToRgb(document.getElementsByName("middlecolour")[0].value)
     let c = hexToRgb(document.getElementsByName("secondcolour")[0].value)
-    let slider = document.getElementById("myRange").value
+    let slider = document.getElementById("middleLocation").value
     let rgbList = [
       [],
       [],
       [],
     ];
     for (let l = 0; l < 3; l += 1)
-      for (let i = Object.values(a)[l], j = 0; i <= Object.values(d)[l], j < slider; i += (Object.values(d)[l] - Object.values(a)[l]) / slider, j += 1) rgbList[l].push(i);
+      for (let i = a[l], j = 0; i <= d[l], j < slider; i += (d[l] - a[l]) / slider, j += 1) rgbList[l].push(i);
     for (let l = 0; l < 3; l += 1)
-      for (let i = Object.values(d)[l], j = 0; i >= Object.values(c)[l], j < ((colourCount * 10 + 1) - slider); i -= (Object.values(d)[l] - Object.values(c)[l]) / ((colourCount * 10 + 1) - slider), j += 1) rgbList[l].push(i);
+      for (let i = d[l], j = 0; i >= c[l], j < ((colourCount * 10 + 1) - slider); i -= (d[l] - c[l]) / ((colourCount * 10 + 1) - slider), j += 1) rgbList[l].push(i);
     return `#${[rgbList[0][Math.round(n*10)],rgbList[1][Math.round(n*10)],rgbList[2][Math.round(n*10)]].map(n=>Math.round(n).toString(16).padStart(2,0)).join("")}`
   };
 
   let colourCount = document.getElementById("colourCount").value
-  document.getElementById("myRange").max = colourCount * 10;
+  document.getElementById("middleLocation").max = colourCount * 10;
   for (let l = 0; l <= colourCount * 10; l += 1) {
     const para = document.createElement("div");
     para.appendChild(document.createTextNode(l / 10));
