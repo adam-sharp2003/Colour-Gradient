d550c1cb67a3af0d376a52cd38f5ccd473922b39
Adam Sharp <adam.sharp2003@gmail.com>
2022-06-14 12:21:58 +0100

    Fixed Bottom Colour

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,5 @@
-var preset = ["#b63202","#b22d02","#af2802","#ab2202","#a81d02","#a41803","#a11303","#9d0d03","#9a0803","#960303"]
+var preset = ["#b63202", "#b22d02", "#af2802", "#ab2202", "#a81d02", "#a41803", "#a11303", "#9d0d03", "#9a0803", "#960303"]
+
 function controlsChanged() {
   document.getElementById("controller").innerHTML = "", b = parseInt(document.getElementById("maincolours").value);
   for (let d = 1; d <= b; d += 1) {
@@ -7,9 +8,7 @@ function controlsChanged() {
     t = document.createElement("input");
     t.type = "color", t.name = "colour" + (d + 1), t.setAttribute("onkeyup", "formChanged()"), t.setAttribute("onchange", "formChanged()"), t.setAttribute("value", `#${Math.floor(Math.random()*16777215).toString(16)}`), t.style.cssText = "width:100%; border: none; padding: 0; background-color: black", document.getElementById("controller").appendChild(t);
     t = document.createElement("input");
-    t.type = "range", t.min = "0", t.max = 50, t.setAttribute("value", t.max / (b + 1) * d), t.setAttribute("oninput", "formChanged()"), t.setAttribute("id", "middleLocation" + d), t.style.cssText = "width:95%; float: left; margin-right: 10px", document.getElementById("controller").appendChild(t);
-    t = document.createElement("p");
-    t.setAttribute("id", "middleInt" + d), document.getElementById("controller").appendChild(t)
+    t.type = "range", t.min = "0", t.max = 50, t.setAttribute("value", t.max / (b + 1) * d), t.setAttribute("oninput", "formChanged()"), t.setAttribute("id", "middleLocation" + d), t.style.cssText = "width:100%; float: left; margin-right: 10px", document.getElementById("controller").appendChild(t);
   }
 }
 
@@ -29,10 +28,7 @@ function formChanged() {
     let d = []
     for (let l = 1; l <= 1 + b; l += 1) a.push(hexToRgb(document.getElementsByName("colour" + l)[0].value))
     a.push(hexToRgb(document.getElementsByName("bottomcolour")[0].value))
-    for (let l = 1; l <= b; l += 1) {
-      c.push(Math.round(document.getElementById("middleLocation" + l).value / l))
-      d.push(Math.round((colourCount - c[l - 1] / b + 1)))
-    }
+    for (let l = 1; l <= b; l += 1) c.push(Math.floor(document.getElementById("middleLocation" + l).value / l))
     let rgbList = [
       [],
       [],
@@ -41,18 +37,15 @@ function formChanged() {
     for (let k = 0; k < b; k += 1)
       for (let l = 0; l < 3; l += 1)
         for (let i = a[k][l], j = 0; i <= a[k + 1][l], j < c[k]; i += (a[k + 1][l] - a[k][l]) / c[k], j += 1) rgbList[l].push(i);
+    let e = colourCount - rgbList[0].length + 1
     for (let l = 0; l < 3; l += 1)
-      for (let i = a[b][l], j = 0; i >= a[b + 1][l], j < d[b - 1]; i -= (a[b][l] - a[b + 1][l]) / d[b - 1], j += 1)
-        if(j % b == 0)rgbList[l].push(i);
-    console.log(rgbList, d, c);
+      for (let i = a[b][l], j = 0; i <= a.at(-1)[l], j < e; i += (a.at(-1)[l] - a[b][l]) / e, j += 1) rgbList[l].push(i);
     return `#${[rgbList[0][Math.round(n*10)],rgbList[1][Math.round(n*10)],rgbList[2][Math.round(n*10)]].map(n=>Math.round(n).toString(16).padStart(2,0)).join("")}`
   };
+
   var b = parseInt(document.getElementById("maincolours").value)
   var colourCount = document.getElementById("colourCount").value * 10
-  for (let l = 1; l <= b; l += 1) {
-    document.getElementById("middleInt" + l).innerHTML = document.getElementById("middleLocation" + l).value / 10
-    document.getElementById("middleLocation" + l).max = colourCount;
-  }
+  for (let l = 1; l <= b; l += 1) document.getElementById("middleLocation" + l).max = colourCount;
   document.getElementById("colourCountInt").innerHTML = colourCount / 10
   for (let l = 0; l <= colourCount; l += 1) {
     const para = document.createElement("div");
